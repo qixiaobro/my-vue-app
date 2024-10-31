c20484ca2201151db8631f29811cab18548624ea
qixiaobro <zxd982092690@gmail.com>
2024-10-31 21:07:21 +0800

    test

--- src/sw.js
+++ src/sw.js
@@ -23,5 +23,29 @@ registerRoute(new NavigationRoute(
   { allowlist },
 ))
 
+const VERSION = '1.0.0' // 每次更新时修改版本号
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    // 强制更新所有客户端
+    Promise.all([
+      // 清理旧缓存
+      cleanupOutdatedCaches(),
+      // 更新所有客户端
+      clients.claim(),
+      // 可选: 清理其他缓存
+      caches.keys().then(keys => {
+        return Promise.all(
+          keys.map(key => {
+            if (key.includes('workbox-') && !key.includes(VERSION)) {
+              return caches.delete(key)
+            }
+          })
+        )
+      })
+    ])
+  )
+})
+
 self.skipWaiting()
 clientsClaim()
