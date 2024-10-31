44acc10a4cbe0ef0ebdfd334e448f33d403dbaa2
qixiaobro <zxd982092690@gmail.com>
2024-10-31 20:48:42 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -21,6 +21,24 @@ export default defineConfig({
       short_name: 'vpd',
       description: 'pwa demo projects',
       theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable'
+        }
+      ]
     },
 
     injectManifest: {
